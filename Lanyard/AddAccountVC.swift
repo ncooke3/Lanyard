b1c090aa8a5ff231a3a62b32dee9e5dd324682a9
Nicholas Cooke <ncooke3@gatech.edu>
2019-01-12 23:24:34 -0500

    added inline company search bar

--- Lanyard/AddAccountVC.swift
+++ Lanyard/AddAccountVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Hero
+import Alamofire
+import SwiftyJSON
 
 extension UIViewController {
     /// Dismisses open keyboards by tapping anywhere in vc
@@ -28,7 +30,7 @@ extension UIViewController {
     /// Dismisses keyboard when 'return' is tapped
     @objc func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        //self.account.textColor = UIColor.black
+        //self.accountField.textColor = UIColor.black
         return false
     }
     
@@ -45,11 +47,24 @@ extension UIViewController {
 
 class AddAccountVC: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet var askUser: UILabel!
+    var askUser: UILabel!
 
-    @IBOutlet var account: UITextField!
+    let accountField = UISuggestionTextField()
     
     var accountName:String = ""
+    
+    var currentSuggestion: String = "" {
+        didSet {
+            accountField.suggestionText = currentSuggestion
+        }
+    }
+    
+    
+    private var request: Request? {
+        didSet {
+            oldValue?.cancel()
+        }
+    }
 
     override func viewDidLoad() {
 
@@ -58,8 +73,6 @@ class AddAccountVC: UIViewController, UITextFieldDelegate {
         let layer = CAGradientLayer()
         layer.frame = view.bounds
         layer.colors = [ blue.cgColor, UIColor.white.cgColor]
-        //layer.startPoint = CGPoint(x: 0, y: 0)
-        //layer.endPoint = CGPoint(x: 1, y: 1)
         view.layer.addSublayer(layer)
         
         navigationItem.hidesBackButton = true
@@ -73,6 +86,23 @@ class AddAccountVC: UIViewController, UITextFieldDelegate {
         self.setupAccountTextEdit()
         
         self.devBorders(devBordersOn: false)
+        
+        self.accountField.addTarget(self, action: #selector(textDidChange(textField:)), for: .editingChanged)
+        
+    }
+    
+    var userText = ""
+    
+    @objc private func textDidChange(textField: UITextField) {
+       self.accountField.textChanged()
+        
+        userText = textField.text ?? ""
+        
+        if userText.lowercased() != currentSuggestion.lowercased().dropLast(max(currentSuggestion.count - userText.count, 0)) {
+            currentSuggestion = ""
+        }
+        
+        setRecommendation(for: userText)
     }
     
     @objc func createAskUser() {
@@ -100,122 +130,71 @@ class AddAccountVC: UIViewController, UITextFieldDelegate {
     
     @objc func setupAccountTextEdit() {
         
-        account = UITextField()
+        accountField.translatesAutoresizingMaskIntoConstraints = false
         
-        account.translatesAutoresizingMaskIntoConstraints = false
+        accountField.tintColor = UIColor.lightGray
+        accountField.setIcon(#imageLiteral(resourceName: "icon-account"))
         
-        account.tintColor = UIColor.lightGray
-        account.setIcon(#imageLiteral(resourceName: "icon-account"))
+        accountField.backgroundColor = .white
+        accountField.placeholder = "Account"
+        accountField.layer.cornerRadius = 7.0
         
-        account.backgroundColor = .white
-        account.placeholder = "Account"
-        account.layer.cornerRadius = 7.0
+        self.view.addSubview(accountField)
+        self.accountField.delegate = self
         
-        self.view.addSubview(account)
-        self.account.delegate = self
-        
-        account.widthAnchor.constraint(equalToConstant: 325).isActive = true
-        account.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        account.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        account.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        accountField.widthAnchor.constraint(equalToConstant: 325).isActive = true
+        accountField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        accountField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        accountField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     
-    
-    
-    
-    /// Implementing Inline Autocomplete
-    
-    var autoCompletePossibilities = ["Apple", "Pineapple", "Orange"]
-    var autoCompleteCharacterCount = 0
-    var timer = Timer()
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        var subString = (textField.text!.capitalized as NSString).replacingCharacters(in: range, with: string)
-        subString = formatSubstring(subString: subString)
-        
-        if subString.count == 0 {
-            resetValues()
-        } else {
-            searchAutocompleteEntriesWithSubstring(substring: subString)
+    func setRecommendation(for text : String) {
+        guard !text.isEmpty else {
+            request?.cancel()
+            currentSuggestion = ""
+            return
         }
-        return true
-    }
-    
-    func formatSubstring(subString: String) -> String {
-        let formatted = String(subString.dropLast(autoCompleteCharacterCount)).lowercased().capitalized
-        return formatted
-    }
-    
-    func resetValues() {
-        autoCompleteCharacterCount = 0
-        account.text = ""
-    }
-    
-    func searchAutocompleteEntriesWithSubstring(substring: String) {
-        let userQuery = substring
-        let suggestions = getAutocompleteSuggestions(userText: substring)
         
-        if suggestions.count > 0 {
-            timer = .scheduledTimer(withTimeInterval: 0.01, repeats: false, block: { (Timer) in
-                let autocompleteResult = self.formatAutocompleteResult(substring: substring, possibleMatches: suggestions)
-                self.putColourFormattedTextInTextField(autocompleteResult: autocompleteResult, userQuery: userQuery)
-                self.moveCaretToEndOfUserQueryPosition(userQuery: userQuery)
-                })
-        } else {
-            timer = .scheduledTimer(withTimeInterval: 0.01, repeats: false, block: { (Timer) in
-                self.account.text = substring
-            })
-            
-            autoCompleteCharacterCount = 0
-        }
-    }
-    
-    func getAutocompleteSuggestions(userText: String) -> [String] {
-        var possibleMatches: [String] = []
-        for item in autoCompletePossibilities {
-            let mystring:NSString! = item as NSString
-            let substringRange:NSRange! = mystring.range(of: userText)
-            
-            if (substringRange.location == 0) {
-                possibleMatches.append(item)
+        
+        //make suggestion text label the first bit should be what the user has already written then after that display the rest of the suggestion
+        //when what the user has written doesnt match the suggestion than it will still look correct i.e capital E thing
+        
+        //user types vharacter that doesnt match up with next charcater in suggestion you need to hide suggestion immediately and show (textDidChange if last letter of text != what /// set sugg to empty s
+        
+        
+        request = Alamofire.request("https://autocomplete.clearbit.com/v1/companies/suggest?query=\(text)", method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let companyNames = json.array?.compactMap { $0["name"].string } ?? []
+                self.currentSuggestion = self.recommendedSuggestions(for: companyNames)
+            case .failure(let error):
+                print(error)
             }
         }
-        return possibleMatches
     }
     
-    func putColourFormattedTextInTextField(autocompleteResult: String, userQuery: String) {
-        let colouredString: NSMutableAttributedString = NSMutableAttributedString(string: userQuery + autocompleteResult)
-        colouredString.addAttributes([NSAttributedString.Key.foregroundColor : UIColor.lightGray], range: NSRange(location: userQuery.count, length: autocompleteResult.count))
-        self.account.attributedText = colouredString
-    }
-    
-    func moveCaretToEndOfUserQueryPosition(userQuery : String) {
-        if let newPosition = self.account.position(from: self.account.beginningOfDocument, offset: userQuery.count) {
-            self.account.selectedTextRange = self.account.textRange(from: newPosition, to: newPosition)
+    func recommendedSuggestions(for suggestions: [String]) -> String {
+        guard !userText.isEmpty else {
+            return ""
         }
-        let selectedRange: UITextRange? = account.selectedTextRange
-        account.offset(from: account.beginningOfDocument, to: (selectedRange?.start)!)
+        return suggestions.first { $0.lowercased().hasPrefix(userText.lowercased()) } ?? ""
+    }
+    
+
+    @objc override func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.text = currentSuggestion
+        return false
     }
 
-    
-    func formatAutocompleteResult(substring: String, possibleMatches: [String]) -> String {
-        var autoCompleteResult = possibleMatches[0]
-    autoCompleteResult.removeSubrange(autoCompleteResult.startIndex..<autoCompleteResult.index(autoCompleteResult.startIndex, offsetBy: substring.count))
-        autoCompleteCharacterCount = autoCompleteResult.count
-        return autoCompleteResult
-    }
-    
-    /// End Implementation
-    
-    
-    
     
     
     ///Add Button Action
     @objc func nextButtonAction(_ sender: UIBarButtonItem!) {
         
-        accountName = account.text!
+        accountName = accountField.text!
         
         let userVC = AddUserVC()
         userVC.hero.isEnabled = true
